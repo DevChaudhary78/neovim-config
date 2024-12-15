1565ba44bb953941c7d88eb6f51cc65a877fed56
Dev Chaudhary <devchaudhary20@proton.me>
2024-12-15 16:03:17 -0500

    testing

--- init.lua
+++ init.lua
@@ -28,6 +28,7 @@ vim.opt.clipboard = 'unnamedplus'
 -- Separate delete and cut command
 local opts = { noremap = true, silent = true }
 
+-- Separate out cut and delete
 -- Normal mode mappings
 vim.keymap.set('n', 'd', '"_d', opts)
 vim.keymap.set('n', 'D', '"_D', opts)
@@ -184,38 +185,6 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
     },
-  },
-
-  -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
-  --
-  -- This is often very useful to both group configuration, as well as handle
-  -- lazy loading plugins that don't need to be loaded immediately at startup.
-  --
-  -- For example, in the following configuration, we use:
-  --  event = 'VimEnter'
-  --
-  -- which loads which-key before all the UI elements are loaded. Events can be
-  -- normal autocommands events (`:help autocmd-events`).
-  --
-  -- Then, because we use the `config` key, the configuration only runs
-  -- after the plugin has been loaded:
-  --  config = function() ... end
-
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-      }
-    end,
   },
 
   -- NOTE: Plugins can specify dependencies.
@@ -714,6 +683,12 @@ require('lazy').setup({
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.files').setup {
+        windows = {
+          preview = true,
+        },
+        options = {
+          permanent_delete = true,
+        },
         vim.keymap.set('n', '<C-n>', function()
           MiniFiles.open()
         end, { noremap = true, silent = true }),
--- lua/custom/plugins/quality.lua
+++ lua/custom/plugins/quality.lua
@@ -1,33 +1,5 @@
 return {
   {
-    'folke/noice.nvim',
-    event = 'VeryLazy',
-    config = function()
-      require('noice').setup {
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-            ['vim.lsp.util.stylize_markdown'] = true,
-            ['cmp.entry.get_documentation'] = true, -- requires hrsh7th/nvim-cmp
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-      }
-    end,
-    dependencies = {
-      'MunifTanjim/nui.nvim',
-      'rcarriga/nvim-notify',
-    },
-  },
-  {
     'windwp/nvim-autopairs',
     dependencies = { 'hrsh7th/nvim-cmp' },
     config = function()
@@ -147,5 +119,25 @@ return {
     config = function()
       vim.g.cutlass_override_del = 1
     end,
+  },
+  {
+    'folke/snacks.nvim',
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+      bigfile = { enabled = true },
+      dashboard = { enabled = true },
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+      rename = { enabled = true },
+    },
   },
 }
