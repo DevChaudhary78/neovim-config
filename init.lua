f3053ac9b942a382e4c31cb06205ea32e3d5f72a
Dev Chaudhary <devchaudhary20@proton.me>
2024-12-20 18:26:03 -0500

    Added clangd compiler flags to suppress warning for missing <stdio.h> header file

--- init.lua
+++ init.lua
@@ -458,6 +458,8 @@ require('lazy').setup({
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
+      capabilities.offsetEncoding = 'utf=8'
+
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --
@@ -469,10 +471,20 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {
-          cmd = { 'clangd', '--log=verbose', 'C:\\msys64\\mingw64\\bin\\gcc.exe' },
+          cmd = { 'clangd', '--log=verbose', '--query-driver=C:\\Program Files\\LLVM\\bin\\clang.exe, C:\\msys64\\mingw64\\bin\\gcc.exe' },
+          init_options = {
+            clangdFileStatus = true,
+            fallbackFlags = {
+
+              '-I',
+              'C:\\msys64\\mingw64\\include',
+              '-I',
+              'C:\\msys64\\mingw64\\lib\\gcc\\x86_64-w64-mingw32\\14.2.0\\include',
+            },
+          },
         },
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
